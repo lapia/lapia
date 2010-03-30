33fcc353c01a914fa050d3b3441c8853c07a83a3
Lapia <isaakay111@yahoo.com>
2010-03-30 13:27:33 +0300

    0.42

--- html/test.php
+++ html/test.php
@@ -1,25 +1,25 @@
-<?php
-/*
- *      test.php
- *
- *      Copyright 2010  <test@dell28>
- *
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
- *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
- */
-?>
+<!--
+        test.php
+
+        Copyright 2010  <test@dell28>
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+        MA 02110-1301, USA.
+-->
+<?php session_start();
+ini_set('display_errors',1);?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -33,28 +33,11 @@
 
 <body>
 
-	<?php
-	$username = 'Zenon'
-
-	$con = mysql_connect("localhost","root","test1");
-	if (!$con)
-	  {
-	  die('Could not connect: ' . mysql_error());
-	  }
-
-	mysql_select_db("LHR", $con);
-
-	$result = mysql_query("SELECT * FROM registereduser WHERE Contactperson = '$username'");
-
-	while($row = mysql_fetch_array($result))
-	  {
-	  echo $row['RegisteredEmailaddress'] . " " . $row['password'];
-	  echo "<br />";
-	  }
-
-	mysql_close($con);
-	?>
-
+<form action="test2.php" method="post">
+Name: <input type="text" name="name" value="<?php echo $_SESSION ["xxx"];//$raw = $_SESSION["raw"]; echo $raw["Contactperson"]; ?>"><br>
+Password: <input type="text" name="passwd" value="<? echo $passwd; ?>"><br>
+<input type="Submit" value="Update">
+</form>
 
 </body>
 </html>
